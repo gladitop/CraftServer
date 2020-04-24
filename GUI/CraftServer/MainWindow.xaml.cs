de880301047946d0d6818c83aa4fbd9bbb1a1390
Gladi <dam.almaev@gmail.com>
2020-04-24 14:47:39 +0300

    Много картинок!
    + Создал класс Data
    + Создал класс Settings
    + Добавил json (скоро будет в описании)
    ++++ Добавил кучу картинок
    + Начал делать дизайн

--- GUI/CraftServer/MainWindow.xaml.cs
+++ GUI/CraftServer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CraftServer.Other.Class;
 
 namespace CraftServer
 {
@@ -23,6 +24,10 @@ namespace CraftServer
         public MainWindow()
         {
             InitializeComponent();
+
+            //Настройки
+
+            Data.Settings = SettingsManager.Load();
         }
     }
 }
---
+++ GUI/CraftServer/Other/Class/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftServer.Other.Class
+{
+    static public class Data
+    {
+        #region Параметры
+
+        public const string PathSave = "Settings.json";//Файл сохранение
+
+        #endregion
+
+        #region Переменные
+
+        static public Settings Settings { get; set; }//Настройки
+
+        #endregion
+    }
+}
